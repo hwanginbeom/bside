b9c0a1d05289c89df624d15b42a8088fef9d8338
kwon_dae <eodud4976@gmail.com>
2022-01-26 13:51:34 +0900

    front_api

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,9 @@
 //frontend/src/app.js
 import React, { Component } from 'react';
+import GoogleLogin from 'react-google-login';
+import axios from 'axios'
 
+const clientId = "774738321836-vl1oi9dvoefg5mpjhijoaic2ond37l2a.apps.googleusercontent.com";
 class App extends Component {
     state = {
         posts: []
@@ -32,4 +35,38 @@ class App extends Component {
     }
 }
 
-export default App;
+function GoogleLoginBtn({ onGoogleLogin }) {
+
+    const onSuccess = async (res) => {
+        console.log(res)
+
+        const { googleId, profileObj: { email, name },} = res
+
+        let data = {
+            GoogleId : googleId,
+            Email : email,
+            Name : name,
+            Provider : 'google'
+        };       
+        
+        console.log(data)    
+        
+        axios.post('http://127.0.0.1:8000/api/login/', data).then(res => {
+            console.log(res)
+        })
+    }
+    const onFailure = (error) => {
+        console.log(error)
+    }
+    return <div>
+        <GoogleLogin
+            clientId={clientId}
+            responseType={'id_token'}
+            onSuccess={onSuccess}
+            onFailure={onFailure}
+        />
+  </div>;
+}
+
+
+export default GoogleLoginBtn;

153e795f16d268adda77d8e397abe2c346c3c80b
hwanginbeom <rydn2002@gmail.com>
2022-02-03 21:51:10 +0900

    21.02.03 | static

--- backend/mysite/config/settings.py
+++ backend/mysite/config/settings.py
@@ -14,9 +14,9 @@ from pathlib import Path
 import os
 import pymysql
 pymysql.install_as_MySQLdb()
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+ROOT_DIR = os.path.dirname(BASE_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -136,13 +136,12 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.1/howto/static-files/
-
 STATIC_URL = '/static/'
-
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'config', 'static')
+    STATIC_DIR,
 ]
+STATIC_ROOT = os.path.join(ROOT_DIR, '.static_root')
+
 
 AUTH_USER_MODEL = 'pybo.User'

a6a2e0f525eb96dac569e7563348c70a12bb79c9
kwon_dae <eodud4976@gmail.com>
2022-03-01 18:32:30 +0900

    agenda, action column_update

--- backend/mysite/pybo/urls.py
+++ backend/mysite/pybo/urls.py
@@ -14,11 +14,11 @@ router.register('secession', views.SecessionSerializer)
 
 urlpatterns = [
     path('', include(router.urls)),
-    re_path('^meets/(?P<meet_status>.+)/$', views.MeetsList.as_view()),
-    re_path('^meets/(?P<rm_status>.+)/$', views.MeetsList.as_view()),
-    re_path('^agendas/(?P<meet_id>.+)/$', views.AgendasList.as_view()),
-    re_path('^actions/(?P<agenda_id>.+)/$', views.ActionsList.as_view()),
-    re_path('^selfchecks/(?P<meet_id>.+)/$', views.SelfChecksList.as_view()),
+    # re_path('^meets/(?P<meet_status>.+)/$', views.MeetsList.as_view()),
+    # re_path('^meets/(?P<rm_status>.+)/$', views.MeetsList.as_view()),
+    # re_path('^agendas/(?P<meet_id>.+)/$', views.AgendasList.as_view()),
+    # re_path('^actions/(?P<agenda_id>.+)/$', views.ActionsList.as_view()),
+    # re_path('^selfchecks/(?P<meet_id>.+)/$', views.SelfChecksList.as_view()),
 
 ]
 
